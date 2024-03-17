315dd86f8b2aa5d5d2af5b5eb44383d287d66605
“emansarahafi” <emansarahafi@gmail.com>
2024-03-17 22:22:01 +0100

    Small touch

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private var btnSubmit: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
