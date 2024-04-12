38032ae18ff95725b6a73ef2fd6321e01c89ad09
“emansarahafi” <emansarahafi@gmail.com>
2024-04-12 21:32:46 +0100

    Lab 11 fixed warnings.

--- app/src/main/java/com/example/firstapplication/ResultActivity.kt
+++ app/src/main/java/com/example/firstapplication/ResultActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,13 +13,13 @@ class ResultActivity : AppCompatActivity() {
         val tvScore : TextView = findViewById(R.id.tv_score)
         val btnFinish : TextView = findViewById(R.id.btn_finish)
 
-
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS,0)
         val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANS,0)
 
-        tvScore.text = "Your score is $correctAnswers out of $totalQuestions"
+        val scoreText = getString(R.string.your_score_is_9_out_of_11, correctAnswers, totalQuestions)
+        tvScore.text = scoreText
 
-        btnFinish.setOnClickListener(){
+        btnFinish.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
